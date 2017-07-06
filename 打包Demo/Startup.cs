603da8110081ca986eaf0b78e9a749ad6e761c6d
charles.he <charles0525@sina.cn>
2017-07-06 23:46:45 +0800

    初始化

---
+++ 打包Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(打包Demo.Startup))]
+namespace 打包Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
